7e1eff12bd4bdeec568f4ebedbf1c6b48f4fef3d
free4mOriginal <free4m@gmail.com>
2019-05-29 20:06:18 -0500

    Add extra blog entries

--- pages/Blog.js
+++ pages/Blog.js
@@ -49,6 +49,20 @@ ${state.squares}
             </figure>
         </div>
 
+        <div class="">
+        <figure>
+            <img src="https://picsum.photos/200?random7" width="200" height="200" alt="random image">
+            <figcaption>Image caption goes here</figcaption>
+        </figure>
+    </div>
+
+    <div class="">
+        <figure>
+            <img src="https://picsum.photos/200?random8" width="200" height="200" alt="random image">
+            <figcaption>Image caption goes here</figcaption>
+        </figure>
+    </div>
+
     </div>
 
 </div>
