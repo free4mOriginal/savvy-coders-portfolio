57ae9d21638f049ca99f0a40be9d607af86557a4
free4mOriginal <free4m@gmail.com>
2019-05-30 21:04:09 -0500

    Remove axios rimport

--- pages/Blogitem.js
+++ pages/Blogitem.js
@@ -1,5 +1,3 @@
-// import axios from 'axios';
-
 export default (state) => `
 <figure>
     <img src="https://picsum.photos/200?random${Math.floor(Math.random() * 100)}" width="200" height="200" alt="random image">
