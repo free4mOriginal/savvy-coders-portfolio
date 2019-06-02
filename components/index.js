ba6ca3de90dc039496bab4707a95a97744adb865
free4mOriginal <free4m@gmail.com>
2019-06-02 12:00:03 -0500

    Add Promise.reject(new Error()) to the fetch chain

--- components/index.js
+++ components/index.js
@@ -9,7 +9,12 @@ const root = document.querySelector('body > div');
 
 const caption = () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((response) => response.json())
+        .then((response) => {
+            if (!response.ok) {
+                return Promise.reject(new Error('Bad request'));
+            }
+            return response.json();
+        })
         .then((resJSON) => {
             for (let i = 0; i < 100; i++) {
                 states.Blog.text.push(resJSON[Math.floor(Math.random() * 100)].body);
