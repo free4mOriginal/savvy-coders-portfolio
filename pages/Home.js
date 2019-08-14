e475a9cd5216bef14236ee53a2cec2bcf107bd69
free4mOriginal <free4m@gmail.com>
2019-08-14 16:49:53 -0500

    more styling 3

--- pages/Home.js
+++ pages/Home.js
@@ -12,7 +12,7 @@ export default state => `${state.squares}
         <figure>
             <a href="https://tyroapp-2567e.firebaseapp.com/" target="_blank">
             <img src="${state.images.tyro}" alt="Tyro React web app">
-            <figcaption>TYRO social media SPA • ReactJS, Redux, Express, MaterialUI</figcaption>
+            <figcaption>TYRO social media SPA • ReactJS, Redux, Express, MaterialUI (Chrome-only)</figcaption>
             </a>
         </figure>
         <figure>
