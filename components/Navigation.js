02b9aa675d03e9d0a76d0b088821ed7f90b4ee05
free4mOriginal <free4m@gmail.com>
2019-11-14 12:40:33 -0600

    fix nav links on mobile

--- components/Navigation.js
+++ components/Navigation.js
@@ -9,13 +9,13 @@ export default (state) => `<nav class="flex flex--justify">
     <p class="navTitles">Portfolio</p>
     <hr>
     <p>
-        <a href="https://reactris.netlify.com/" target="_blank">REACTRIS game</a><BR>
-        <a href="https://beer-bank.netlify.com/" target="_blank">Beer Bank SPA</a><BR>
-        <a href="https://tyroapp-2567e.firebaseapp.com/" target="_blank">TYRO Social Media SPA</a><BR>
-        <a href="https://courselist.netlify.com/" target="_blank">Searchable Courselist</a><BR>
-        <a href="https://karinaliner.netlify.com/" target="_blank">Capstone Project</a><BR>
-        <a href="https://jammming.netlify.com/" target="_blank">Spotify API web app</a><BR>
-        <a href="http://www.free4m.com/" target="_blank">Fine Art & Design</a>
+        <a href="https://reactris.netlify.com/" target="_blank">REACTRIS</a><BR>
+        <a href="https://beer-bank.netlify.com/" target="_blank">Beer Bank</a><BR>
+        <a href="https://tyroapp-2567e.firebaseapp.com/" target="_blank">TYRO Social</a><BR>
+        <a href="https://courselist.netlify.com/" target="_blank">Courselist</a><BR>
+        <a href="https://karinaliner.netlify.com/" target="_blank">Artist App</a><BR>
+        <a href="https://jammming.netlify.com/" target="_blank">Spotify API</a><BR>
+        <a href="http://www.free4m.com/" target="_blank">Fine Art</a>
     </p>
 </figure>
 
